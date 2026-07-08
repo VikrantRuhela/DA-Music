9e9b7ad580088ff968426cd5fed448de3e873ebf
Vikrant Ruhela <113286905+VikrantRuhela@users.noreply.github.com>
2026-07-08 18:05:07 +0530

    Delete scratch directory

--- scratch/print_yt_clients.dart
+++
@@ -1,8 +0,0 @@
-import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yt;
-
-void main() {
-  print('YoutubeApiClient values:');
-  for (final value in yt.YoutubeApiClient.values) {
-    print('  - $value');
-  }
-}
